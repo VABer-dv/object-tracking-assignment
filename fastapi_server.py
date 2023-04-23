fc423ee44ebac33f927edcc519bc680df00e8585
German Leontiev <german_leontiev@mail.ru>
2023-04-23 21:43:39 +0300

    soft_tracker added

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, WebSocket
-from track_3 import track_data, country_balls_amount
+from track_4 import track_data, country_balls_amount
 import asyncio
 import glob
+import numpy as np
+import json
 
 app = FastAPI(title='Tracker assignment')
 imgs = glob.glob('imgs/*')
@@ -9,7 +11,11 @@ country_balls = [{'cb_id': x, 'img': imgs[x % len(imgs)]} for x in range(country
 print('Started')
 
 
-def tracker_soft(el):
+def dist(a, b):
+    return sum(abs(np.array(a) - np.array(b)))
+
+
+def tracker_soft(el, new_obj, last_frame):
     """
     Необходимо изменить у каждого словаря в списке значение поля 'track_id' так,
     чтобы как можно более длительный период времени 'track_id' соответствовал
@@ -24,7 +30,29 @@ def tracker_soft(el):
     вашего трекера, использовать его в алгоритме трекера запрещено
     - запрещается присваивать один и тот же track_id разным объектам на одном фрейме
     """
-    return el
+
+    # Проходимся по новому объекту и присваиваем новый id на первом фрейме
+    if el["frame_id"] == 1:
+        for obj in el["data"]:
+            obj["track_id"] = new_obj
+            new_obj += 1
+        # Перед выходом запомним послежний фрейм
+        last_frame = el
+        return el, new_obj, last_frame
+
+    # Если фрейм уже не первый...
+    # Смотрим предыдущий фрейм
+    last_data = {i["track_id"]: i["bounding_box"] for i in last_frame["data"] if i["bounding_box"]}
+    for obj in el["data"]:
+        bb = obj["bounding_box"]
+        if not last_data:
+            obj["track_id"] = new_obj
+            new_obj += 1
+        if bb and last_data:
+            idx_min = np.argmin([dist(i, bb) for i in last_data.values()])
+            obj["track_id"] = list(last_data.keys())[idx_min]
+            _ = last_data.pop(obj["track_id"])
+    return el, new_obj, last_frame
 
 
 def tracker_strong(el):
@@ -58,12 +86,26 @@ async def websocket_endpoint(websocket: WebSocket):
     # отправка служебной информации для инициализации объектов
     # класса CountryBall на фронте
     await websocket.send_text(str(country_balls))
+    created_track = []
+    new_obj = 0
+    last_frame = None
     for el in track_data:
         await asyncio.sleep(0.5)
-        # TODO: part 1
-        el = tracker_soft(el)
+        el, new_obj, last_frame = tracker_soft(el, new_obj, last_frame)
+        created_track.append(el)
         # TODO: part 2
         # el = tracker_strong(el)
         # отправка информации по фрейму
         await websocket.send_json(el)
+
+    for frame in created_track:
+        list_of_ids = [i["track_id"] for i in frame["data"] if i["track_id"]]
+        if len(list_of_ids) != len(set(list_of_ids)):
+            print("ERROR!!!", frame["frame_id"], "Frame has the same ids")
+
+    with open("created_track.json", "w") as f:
+        json.dump(created_track, f)
+
+
+
     print('Bye..')
